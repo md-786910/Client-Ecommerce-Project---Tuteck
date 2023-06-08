c38e3d683026b5ed72197303acb610b637cfabcc
md ashif reza <76996887+md-786910@users.noreply.github.com>
2023-06-08 19:59:10 +0530

    complete_project_setup_design

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import HomeSlider from "./app/components/Home/HomeSlider";
 import DealOfDay from "./app/components/Home/DealOfDay";
 import Model from "./app/components/Model";
 import ScrollTop from "./app/components/ScrollTop";
+import SingleProduct from "./app/components/Product/SingleProduct";
 
 function App() {
   return (
     <div className="App-container">
       <Header />
       <HomeSlider />
+      <SingleProduct />
       <Model />
       <DealOfDay />
       <Footer />
