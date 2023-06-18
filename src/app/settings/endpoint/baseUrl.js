39c13f871ff0b749fab858f68cfae297451b507d
md ashif reza <76996887+md-786910@users.noreply.github.com>
2023-06-18 17:18:30 +0530

    Update baseUrl.js

--- src/app/settings/endpoint/baseUrl.js
+++ src/app/settings/endpoint/baseUrl.js
@@ -2,5 +2,5 @@
 export const productUrl = "https://tuteck-server.onrender.com/api/product";
 
 // export const api = "http://localhost:5000/api";
-// export const api = "https://tuteck-server.onrender.com/api";
-export const api = "http://localhost:5000/api";
+export const api = "https://tuteck-server.onrender.com/api";
+// export const api = "http://localhost:5000/api";
